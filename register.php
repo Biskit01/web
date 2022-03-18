f44d9ece40024e2de56c4d774a3b0ad19dc6279f
Biskit01 <kittina@ncv.ac>
2022-03-18 13:35:36 +0700

    a

--- register.php
+++ register.php
@@ -1,3 +1,4 @@
+
 <?php 
     session_start();
     include('connect.php'); 
@@ -8,49 +9,70 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css">
     <title>Register Page</title>
 
-    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="login.css">
 </head>
 <body>
     
-    <div class="header">
-        <h2>Register</h2>
+<div class="container">
+    <input type="checkbox" id="flip">
+    <div class="cover">
+      <div class="front">
+       <img src="img/001.png" alt="">
+        <div class="text">
+          <span class="text-1">เว็บไซต์ระบบสารเทศ<br> วิชาโครงงารและวิชาโครงการ<br>
+         แผนกเทคโนโลยีดิจิทัล</span>
+        </div>
+      </div>
     </div>
 
-    <form action="register_db.php" method="post">
-        <?php include('errors.php'); ?>
-        <?php if (isset($_SESSION['error'])) : ?>
+
+
+
+    <div class="forms">
+        <div class="form-content">
+          <div class="login-form">
+            <div class="title">Register</div>
+          <form action="register_db.php" method="post">
+            <div class="input-boxes">
+            <div class = "sizebox">
+          <?php if (isset($_SESSION['error'])) : ?>
             <div class="error">
-                <h3>
+                <h7>
                     <?php 
                         echo $_SESSION['error'];
                         unset($_SESSION['error']);
                     ?>
-                </h3>
+                </h7>
             </div>
+            
         <?php endif ?>
-        <div class="input-group">
-            <label for="username">Username</label>
-            <input type="text" name="username">
         </div>
-        <div class="input-group">
-            <label for="email">Email</label>
-            <input type="email" name="email">
-        </div>
-        <div class="input-group">
-            <label for="password_1">Password</label>
-            <input type="password" name="password_1">
-        </div>
-        <div class="input-group">
-            <label for="password_2">Confirm Password</label>
-            <input type="password" name="password_2">
-        </div>
-        <div class="input-group">
-            <button type="submit" name="reg_user" class="btn">Register</button>
-        </div>
-        <p>Already a member? <a href="login.php">Sign in</a></p>
-    </form>
+        <div class="input-box">
+                <i class="fas fa-user"></i>
+                <input type="text" placeholder="Enter your name" name="username">
+              </div>
+              <div class="input-box">
+                <i class="fas fa-envelope"></i>
+                <input type="text" placeholder="Enter your email" name="email">
+              </div>
+              <div class="input-box">
+                <i class="fas fa-lock"></i>
+                <input type="password" placeholder="Enter your password" name=" password_1">
+              </div>
+              <div class="input-box">
+                <i class="fas fa-lock"></i>
+                <input type="password" placeholder="Confirm  password" name=" password_2">
+              </div>
+              <div class="button input-box">
+              <input type="submit" name="reg_user"value="register">
+              </div>
+              <div class="text sign-up-text">Already have an account? <a href="Login.php">Login now</a></div>
+            </div>
+        </form>
+      </div>
+
 
 </body>
-</html>
